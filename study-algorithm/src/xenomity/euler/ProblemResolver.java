a53435a9db11cb697b8821347090eb5bbd7f0a39
Xenomity a.k.a P-slinc' (이승한) <Xenomity a.k.a P-slinc' ()@192.168.0.11>
2015-02-01 20:24:23 +0900

    add: euler no.12 (엄청 오래 걸림...;;; 대책 필요!!)

--- study-algorithm/src/xenomity/euler/ProblemResolver.java
+++ study-algorithm/src/xenomity/euler/ProblemResolver.java
@@ -25,7 +25,7 @@ public final class ProblemResolver {
 	}
 	
 	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-		ProblemResolver.resolve(Problem11.class);
+		ProblemResolver.resolve(Problem12.class);
 	}
 	
 }
