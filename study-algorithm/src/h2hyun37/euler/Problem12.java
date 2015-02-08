56361f79c2fb124fa2c83c1ff54e3b718dcbf3f5
h2hyun37 <h2hyun37@gmail.com>
2015-02-08 11:45:54 +0900

    오일로 12 일부 주석 수정

--- study-algorithm/src/h2hyun37/euler/Problem12.java
+++ study-algorithm/src/h2hyun37/euler/Problem12.java
@@ -1,36 +1,33 @@
 package h2hyun37.euler;
 
-import java.util.HashMap;
 import java.util.TreeMap;
 
 public class Problem12 {
-	
 
-	/* 
+
+	/*
 	 * 루프 횟수를 줄일 수 있는 방법
 	 * 1. 루프 돌 때에 소수가 아닌 수는 패스
-	 *     -> 처음에 충분한 만큼의 소수 list 를 가지고 있어야 함 
+	 *     -> 처음에 충분한 만큼의 소수 list 를 가지고 있어야 함
 	 * 2. 2와 홀수만 가지고 루프를 돈다
-	 * 3. 이전 인수분해 결과를 별도로 가지고 있으면서, 인수분해시 비교 
-	 * 
-	 * 실제로 적용한 것은 2번만... 귀찮다 ... 
+	 * 3. 이전 인수분해 결과를 별도로 가지고 있으면서, 인수분해시 비교
+	 * 4. 루프를 root(N) 까지만 돈다
+	 *
+	 * 실제로 적용한 것은 2번만... 귀찮다 ...
 	 */
 
-	// 인수분해한 밑수(?)와 인수를 저장할 map 생성
-	// key : 밑수 , value : 인수
+	// 인수분해한 밑수(?)와 지수를 저장할 map 생성
+	// key : 밑수 , value : 지수
 	private TreeMap<Integer, Integer> treemap = new TreeMap<Integer, Integer>();
 
-	// 소수리스트를 저장할 map 생성
-	private static HashMap<Integer, Integer> primeNumbers = new HashMap<Integer, Integer>(
-			100);
 
 	/*
 	 * 정수를 받아, 해당 정수의 약수 갯수를 받는다
-	 * 
+	 *
 	 * 소인수분해 공식 : a^m * b^n 이고 약수의 갯수는 (m+1)(n+1)
 	 */
 	public int getSubmultipleCount(long num) {
-		
+
 		// 인수분해한 결과를 저장할 map 초기화
 		treemap.clear();
 
@@ -49,23 +46,23 @@ public class Problem12 {
 				int square = treemap.getOrDefault(i, 0);
 				square++;
 				treemap.put(i, square);
-				
+
 				num /= i;
-				i = 1; // 처음부터 다시 인수분해를 해야하므로 i=1 로 세팅한다 
+				i = 1; // 처음부터 다시 인수분해를 해야하므로 i=1 로 세팅한다
 			}
 
 		}
-		
+
 		/*
 		 * 약수의 각 인수+1 한 값들을 곱하여 리턴
-		 * 
+		 *
 		 * (a^m)(b^n) -> (m+1)(n+1) 을 구한다
 		 */
 		int countOfSubmultiple = 1;
-		for (Integer value : treemap.values()) { 
+		for (Integer value : treemap.values()) {
 			countOfSubmultiple *= (value+1);
 		}
-		
+
 		return countOfSubmultiple;
 	}
 
