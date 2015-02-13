3b9bb024f18276281f4eb5bbaebdc1da66cb340c
P052400 <neda.air@gmail.com>
2015-02-13 18:44:14 +0900

    느린 코드를 동적 프로그래밍을 변환

---
+++ study-algorithm/src/nedaair/baekjoon/1003.py
@@ -0,0 +1,42 @@
+__author__ = 'P052400'
+
+
+count0 = 0
+count1 = 0
+
+def fibonacci(n) :
+
+    global count0
+    global count1
+
+
+    if n == 0 :
+        count0 =  count0 + 1
+        return 0
+    elif n == 1 :
+        count1 =  count1 + 1
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+
+def printFibonacci(n) :
+
+    global count0
+    global count1
+
+    fibonacci(n)
+
+    print count0, count1
+
+    count0 = 0
+    count1 = 0
+
+
+
+
+printFibonacci(0)
+printFibonacci(1)
+printFibonacci(3)
+
+
