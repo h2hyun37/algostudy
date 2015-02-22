76a0e9c26cdb151cffa161b12644d5dfad1560af
Xenomity a.k.a P-slinc' (이승한) <Xenomity a.k.a P-slinc' ()@192.168.0.11>
2015-02-22 16:46:19 +0900

    update: re-factoring the baekjoon no.1004 problem.

--- study-algorithm/src/xenomity/baekjoon/No1004.scala
+++ study-algorithm/src/xenomity/baekjoon/No1004.scala
@@ -1,50 +1,48 @@
 package xenomity.baekjoon
 
-object No1004 {
-  private class Problem {
-    def resolve(myPoints: Array[Point], planetPoints: Array[Circle]): Int = {
-      var result = 0;
+class No1004 {
+  def resolve(myPoints: Array[Point], planetPoints: Array[Circle]): Int = {
+    var result = 0;
+    
+    for (i <- 0 to planetPoints.length - 1) {
+      var leftSide = Math.pow(myPoints(0).getX() - planetPoints(i).getX(), 2) + Math.pow(myPoints(0).getY() - planetPoints(i).getY(), 2)
+      var rightSide = Math.pow(planetPoints(i).getRadius(), 2)
       
-      for (i <- 0 to planetPoints.length - 1) {
-        var leftSide = Math.pow(myPoints(0).getX() - planetPoints(i).getX(), 2) + Math.pow(myPoints(0).getY() - planetPoints(i).getY(), 2)
-        var rightSide = Math.pow(planetPoints(i).getRadius(), 2)
+      if (leftSide < rightSide) {
+        result += 1
+      } else {
+        leftSide = Math.pow(myPoints(1).getX() - planetPoints(i).getX(), 2) + Math.pow(myPoints(1).getY() - planetPoints(i).getY(), 2)
         
         if (leftSide < rightSide) {
           result += 1
-        } else {
-          leftSide = Math.pow(myPoints(1).getX() - planetPoints(i).getX(), 2) + Math.pow(myPoints(1).getY() - planetPoints(i).getY(), 2)
-          
-          if (leftSide < rightSide) {
-            result += 1
-          }
         }
       }
-      
-      result
     }
-  }
     
-  private class Point(x: Int, y: Int) {
-    def getX() = x
-    def getY() = y
+    result
   }
+}
   
-  private class Circle(x: Int, y: Int, r: Int) {
-    def getX() = x
-    def getY() = y
-    def getRadius() = r
-  }
-  
-  private object AllDone extends Exception {}
-  
-  def main(args: Array[String]) {
-    var problem = new Problem
+private class Point(val x: Int, val y: Int) {
+  def getX() = x
+  def getY() = y
+}
 
-    println(problem.resolve(
+private class Circle(override val x: Int, override val y: Int, r: Int) extends Point(x, y) {
+  def getRadius() = r
+}
+
+private object AllDone extends Exception {}
+
+object No1004Test {
+  def main(args: Array[String]) {
+    var no1004 = new No1004
+
+    println(no1004.resolve(
         Array(new Point(-5, 1), new Point(12, 1)),
         Array(new Circle(1, 1, 8), new Circle(-3, -1, 1), new Circle(2, 2, 2), new Circle(5, 5, 1), new Circle(-4, 5, 1), new Circle(12, 1, 1), new Circle(12, 1, 2))))
         
-    println(problem.resolve(
+    println(no1004.resolve(
         Array(new Point(-5, 1), new Point(5, 1)),
         Array(new Circle(0, 0, 2))))
   }
