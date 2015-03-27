8de99114875ef809c1847b4f0171e5fb13236108
nedaair <neda.air@gmail.com>
2015-03-28 01:28:12 +0900

    add icpc 1007  작업중

---
+++ study-algorithm/src/nedaair/acmicpc/1007.py
@@ -0,0 +1,15 @@
+__author__ = 'nedaair'
+
+import math
+
+
+points1 = [[5,5], [5, -5], [-5, 5], [-5, -5]]
+prints2 = [[-100000, -100000], [100000, 100000]]
+
+
+
+
+print "{0:.6f}".format(math.sqrt(pow(200000, 2) + pow(200000, 2)))
+
+
+
